00d08ffb2b711390198b152149ceaec7c5d5efe2
Keilla Vitória <keillavfb@gmail.com>
2023-12-20 15:12:04 -0300

    configuração inicial

---
+++ src/main/java/br/com/udemy/springapiblog/SpringApiBlogApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.springapiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiBlogApplication.class, args);
+	}
+
+}
